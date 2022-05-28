072bdadb498fb1968dfda63804697b0dc25672e5
Megyn Pettry <105400607+Megytron@users.noreply.github.com>
2022-05-27 18:13:35 -0600

    Challenge pt. 1

--- Basics/001 Conventions and Syntax/ClassExample.cs
+++ Basics/001 Conventions and Syntax/ClassExample.cs
@@ -10,6 +10,11 @@ public class ClassWithVariables
 {
     public int number = 2;
     public string userName = "Bob";
+    public string characterName = "Faedra";
+    public string race = "Dark Elf";
+    public string characterClass = "Rogue";
+    public string int characterLevel = 20;
+    public string int characterHP = 2000
 }
 
 public class ClassWithFunctions
@@ -17,5 +22,9 @@ public class ClassWithFunctions
     public void RunFunction()
     {
         Console.WriteLine("Running Function");
+        Console.WriteLine("There once was a Dark Elf");
+        Console.WriteLine("Her name was Faedra");
+        Console.WriteLine("She liked being a Dark Elf");
+        Console.WriteLine("But she didn't like her name");
     }
 }   
